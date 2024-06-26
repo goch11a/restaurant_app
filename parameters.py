3f0618efb90eefab14849dd4ef0a08a22abae061
goch11a <gochagabitashvili8@gmail.com>
2024-06-26 19:18:45 +0400

    i

---
+++ parameters.py
@@ -0,0 +1,49 @@
+departments = {
+    'admin': [
+        {
+        'initiate restorant': [
+                            'create necessary files',
+                            'check all files',
+                            'add tables number',
+                            'add salary percent',
+                            'add margin percent',
+                            'add comission percent',
+                            ],
+        'user management': [
+                        'add new user',
+                        'edit user',
+                        'delete user',
+                        ], 
+        'accountant management': [
+                                'get financial report',
+                                'get warehouse balance',
+                                'calculate dish cost',
+                                'add new distributor',
+                                'pay debt',
+                                'pay salaries',
+                                ], 
+        'warehouse management': [
+                                'get warehouse balance',
+                                'add new product',
+                                'extract product',
+                                'drop product',
+                                ],
+        'kitchen management': [
+                                'add new dish',
+                                'edit dish',
+                                'delete dish',
+                                'get orders',
+                                'prepare orders',
+                                'give orders',
+                                ], 
+        'waiters management': [
+                                'get order',
+                                'add order to kitchen',
+                                'get order from kitchen',
+                                'give order to client'
+                                'get payment',
+                                ]
+        }
+            ]
+}
+
