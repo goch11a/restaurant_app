476a17b842059886d3715f9ef287cb3ff2d6d4e1
goch11a <gochagabitashvili8@gmail.com>
2024-06-27 19:16:35 +0400

    gocha

---
+++ restaurant.py
@@ -0,0 +1,19 @@
+import os
+import csv
+
+def main():
+    if not os.path.isfile("writer.csv"):
+        header_()
+
+
+
+
+def header_():
+    with open("writer.csv", "w") as file:
+        header = ["username", "password", "role"]
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(header)  
+
+def check_user(suername):
+    ...
+
